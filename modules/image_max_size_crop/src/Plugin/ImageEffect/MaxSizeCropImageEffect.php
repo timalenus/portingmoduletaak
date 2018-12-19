a1943bbe72aac9b844e958e21e9f0f346d466005
timalenus <tim.alenus.y9731@student.hogent.be>
2018-12-19 11:57:30 +0100

    PHPdoc annotaties toegevoegd

--- modules/image_max_size_crop/src/Plugin/ImageEffect/MaxSizeCropImageEffect.php
+++ modules/image_max_size_crop/src/Plugin/ImageEffect/MaxSizeCropImageEffect.php
@@ -5,7 +5,17 @@ use Drupal\Core\Image\ImageInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\image\ConfigurableImageEffectBase;
 
+/**
+ * Snij een afbeelding bij met respect tot een maximum afmeting. Enkel breedte of hoogte nodig.
+ *
+ * @ImageEffect(
+ *   id = "image_max_size_crop",
+ *   label = @Translation("Maximum size crop"),
+ *   description = @Translation("Cropping will remove portions of an image to make it the specified dimensions. This style only resizes when the image dimension(s) is larger than the spedified dimension(s).")
+ */
+
 class MaxSizeCropImageEffect extends ConfigurableImageEffectBase {
+    
 
   public function applyEffect(ImageInterface $image) {
     return TRUE;
